482df15007688bb27f41594f462e4fa361601cc8
Alex Merser <divupweb@gmail.com>
2018-01-18 14:43:25 +0300

    last-mod

--- database-connection.php
+++ database-connection.php
@@ -1,11 +1,13 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: *");
-$servername = "localhost:3306";
-$username = "root";
-$password = "";
+header('Content-Type: text/html; charset=utf-8');
+$servername = "localhost";
+$username = "divup";
+$password = "frostbite2";
 $dbname = "divup";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
+mysqli_set_charset($conn, 'utf8');
 if(!$conn){
     die("Connection failed: " .mysqli_connecet_error());
 }
@@ -13,7 +15,6 @@ if(!$conn){
 $showData = "SELECT * FROM protfolio_items";
 $data = array();
 $result = mysqli_query($conn, $showData);
-
 if(mysqli_num_rows($result) > 0){
     while($row = mysqli_fetch_assoc($result)){
         $data[] = $row;
@@ -23,5 +24,4 @@ if(mysqli_num_rows($result) > 0){
 };
 print json_encode($data);
 mysqli_close($conn);
-echo($outp);
 ?>
